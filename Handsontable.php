9a5ee5816669a2094ffe49deb6d249bb0a02b0d0
Simon Karlen <karlen@tonic.ag>
2017-11-06 09:46:48 +0100

    added styling options, translations

--- Handsontable.php
+++ Handsontable.php
@@ -50,19 +50,27 @@ class Handsontable extends Widget {
 	public $mergeCells = false;
 
 	/**
+	 * @var boolean Show styling options in context menu like "title row" etc.
+	 */
+	public $styling = false;
+
+	/**
 	 * @var array Translations strings of predefined actions
 	 */
 	private $actionStrings = [
-		'row_above'  => 'Insert row above',
-		'row_below'  => 'Insert row below',
-		'col_left'   => 'Insert column on the left',
-		'col_right'  => 'Insert column on the right',
-		'remove_row' => 'Remove row',
-		'remove_col' => 'Remove column',
-		'undo'       => 'Undo',
-		'redo'       => 'Redo',
-		'merge'      => 'Merge cells',
-		'unmerge'    => 'Unmerge cells'
+		'row_above'     => 'Insert row above',
+		'row_below'     => 'Insert row below',
+		'col_left'      => 'Insert column on the left',
+		'col_right'     => 'Insert column on the right',
+		'remove_row'    => 'Remove row',
+		'remove_col'    => 'Remove column',
+		'undo'          => 'Undo',
+		'redo'          => 'Redo',
+		'merge'         => 'Merge cells',
+		'unmerge'       => 'Unmerge cells',
+		'style'         => 'Style',
+		'set_heading'   => 'Set heading',
+		'unset_heading' => 'Unset heading'
 	];
 
 	/**
@@ -109,7 +117,7 @@ class Handsontable extends Widget {
 			$this->clientOptions['contextMenu'] = [
 				'items' => []
 			];
-			$hsep = 1;
+			$hsep                               = 1;
 			foreach ($this->contextMenu as $action) {
 				$string = ArrayHelper::getValue($this->actionStrings, $action);
 				if (is_null($string)) {
@@ -128,10 +136,10 @@ class Handsontable extends Widget {
 		}
 
 		if ($this->mergeCells !== false) {
-			$this->clientOptions['mergeCells'] = $this->mergeCells;
+			$this->clientOptions['mergeCells']                         = $this->mergeCells;
 			$this->clientOptions['contextMenu']['items']['hsep'.$hsep] = '---------';
 			$this->clientOptions['contextMenu']['items']['mergeCells'] = [
-				'name' => new JsExpression('function () {
+				'name'     => new JsExpression('function () {
 					var hot = this,
 						sel = hot.getSelected(),
 						info = hot.mergeCells.mergedCellInfoCollection.getInfo(sel[0], sel[1]);
@@ -148,6 +156,59 @@ class Handsontable extends Widget {
 					hot.render();
 					Handsontable.hooks.run(hot, \'afterChange\', null, \'edit\');
 				}')
+			];
+		}
+		if ($this->styling) {
+			$this->clientOptions['renderer']                             = 'styleRenderer';
+			$this->clientOptions['contextMenu']['items']['hsep'.$hsep++] = '---------';
+			$this->clientOptions['contextMenu']['items']['style']        = [
+				'name'    => Yii::t('simialbi/handsontable/widget', $this->actionStrings['style']),
+				'submenu' => [
+					'callback' => new JsExpression('function (key, options) {}'),
+					'items'    => [
+						[
+							'name'     => new JsExpression('function () {
+								var hot = this,
+									sel = hot.getSelected(),
+									meta = hot.getCellMeta(sel[0], sel[1]);
+								
+								if (!meta.style || !meta.style.heading) {
+									return \''.Yii::t('simialbi/handsontable/widget', $this->actionStrings['set_heading']).'\';
+								}
+								
+								return \''.Yii::t('simialbi/handsontable/widget', $this->actionStrings['unset_heading']).'\';
+							}'),
+							'callback' => new JsExpression('function (key, options) {
+								var hot = this,
+									sel = hot.getSelected(),
+									meta = hot.getCellMeta(sel[0], sel[1]),
+									heading = true;
+									
+								if (!meta.style) {
+								} else {
+									heading = !meta.style.heading;
+								}
+								
+								for (var i = sel[0]; i <= sel[2]; i++) {
+									for (var k = sel[1]; k <= sel[3]; k++) {
+										meta = hot.getCellMeta(sel[0], sel[1]);
+										if (!meta.style) {
+											meta.style = {heading: heading};
+										} else {
+											meta.style.heading = heading;
+										}
+										hot.setCellMeta(i, k, \'style\', meta.style);
+									}
+								}
+								
+								hot.render();
+								
+								return true;
+							}'),
+							'key'      => 'style:heading'
+						]
+					]
+				]
 			];
 		}
 	}
@@ -174,7 +235,25 @@ class Handsontable extends Widget {
 
 		HandsontableAsset::register($view);
 
-		$view->registerJs("var hot$jsId = new $pluginName(jQuery('#$id').get(0), $options)");
+		$view->registerJs("$pluginName.renderers.registerRenderer('styleRenderer', function (hot, td, row, col, prop, value, cellProperties) {
+			td.style = {};
+			
+			$pluginName.renderers.TextRenderer.apply(this, arguments);
+			
+			var meta = hot.getCellMeta(row, col);
+			if (!meta.style) {
+				return td;
+			}
+			
+			if (meta.style.heading) {
+				td.style.fontWeight = 'bold';
+			}
+			
+			return td;
+		});");
+		$view->registerJs("\nvar hot$jsId = new $pluginName(jQuery('#$id').get(0), $options)");
+		$this->registerClientEvents();
+		$view->registerJs("\nhot$jsId.runHooks('afterInit');");
 	}
 
 	/**
